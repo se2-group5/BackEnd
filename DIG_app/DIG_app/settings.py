b2d887afe7be368ffec89b10ca7671065ae9cb67
gacantorm <riosgadavid@gmail.com>
2022-06-27 21:33:10 -0500

    fix Procfile

--- DIG_app/DIG_app/settings.py
+++ DIG_app/DIG_app/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from genericpath import exists
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$t&@qd88w5pm*26hl0wcx69&+(@zcs#qfi#1sx*dp$sn1&z@65'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["https://dig-app-backend.herokuapp.com/"]
 
@@ -86,6 +88,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddlewarw',
 ]
 
 ROOT_URLCONF = 'DIG_app.urls'
@@ -158,7 +161,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
+STATIC_DIRS = (
+    os.path.join(BASE_DIR,'/static/' )
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- DIG_app/DIG_app/urls.py
+++ DIG_app/DIG_app/urls.py
@@ -13,11 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import statistics
+from turtle import st
+from django import conf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from main.viewsets import UserViewSet
 from main import views
+from django.conf import settings
+from django.conf.urls.static import static 
 
 router = routers.DefaultRouter()
 router.register(r'businesses', views.BusinessView, 'business')
@@ -34,4 +39,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     #path( 'tinymce/', include('tinymce.urls') ),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
