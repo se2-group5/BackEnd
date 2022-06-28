c77b847d23670668785035268b293478b60c6096
gacantorm <riosgadavid@gmail.com>
2022-06-27 21:50:22 -0500

    fix Procfile

--- DIG_app/DIG_app/settings.py
+++ DIG_app/DIG_app/settings.py
@@ -181,9 +181,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_WHITELIST = [
+""" CORS_ORIGIN_WHITELIST = [
      'http://localhost:3000'
-]
+] """
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
