211b6880e203ed3a1571bddb6132374fe6a88770
gacantorm <riosgadavid@gmail.com>
2022-06-27 20:23:37 -0500

    fix requirements file

--- DIG_app/DIG_app/settings.py
+++ DIG_app/DIG_app/settings.py
@@ -69,13 +69,12 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'bootstrap4',
-    #'tinymce',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
