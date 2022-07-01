9f68396d7357278f6f9b634192942639420ca9f2
Juan Sebastian Montoya Combita <jsmontoyaco@unal.edu.co>
2022-07-01 13:43:54 -0500

    Fix issue

--- main/views.py
+++ main/views.py
@@ -160,7 +160,7 @@ def make_report(request, business_id):
                 business = get_object_or_404(Business, pk=business_id)
                 business.rating= round(Report.objects.filter(business_id__exact=business_id).aggregate(Avg('rating_business'))['rating_business__avg'],1)
                 business.save()
-                messages.success(business.rating)
+                messages.success('business.rating')
             except Exception:
                 messages.error('Error during implementation')
             return HttpResponseRedirect( reverse("main:biz_profile", args=(business_id, ) ) )
