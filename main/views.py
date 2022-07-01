dcc2d5f1cc9d39ef38f61649f2a32b374507f998
Juan Sebastian Montoya Combita <jsmontoyaco@unal.edu.co>
2022-07-01 13:49:26 -0500

    Update views.py

--- main/views.py
+++ main/views.py
@@ -156,13 +156,9 @@ def make_report(request, business_id):
         form = ReportForm(data=request.POST)
         if form.is_valid():
             form.save()
-            try:
-                business = get_object_or_404(Business, pk=business_id)
-                business.rating= round(Report.objects.filter(business_id__exact=business_id).aggregate(Avg('rating_business'))['rating_business__avg'],1)
-                business.save()
-                messages.success('business.rating')
-            except Exception:
-                messages.error('Error during implementation')
+            business = get_object_or_404(Business, pk=business_id)
+            business.rating= round(Report.objects.filter(business_id__exact=business_id).aggregate(Avg('rating_business'))['rating_business__avg'],1)
+            business.save()
             return HttpResponseRedirect( reverse("main:biz_profile", args=(business_id, ) ) )
         else:
             messages.error(request, "Invalid form!")
